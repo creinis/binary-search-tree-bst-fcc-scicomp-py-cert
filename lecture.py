40256752fcee34aceee32959b8d74b60673a4028
creinis <115181527+creinis@users.noreply.github.com>
2024-08-02 12:17:19 +0200

    feat: else clause: within an if statement to check if the left child of the current node is None. When node.left is None, there is no left child. Therefore, return the right child from the new if block as a replacement; elif clause to check if the right child of the current node (node.right) is None. If the previous condition is met, it means there is no right child. So, return the left child of the current node from the elif block as a replacement

--- lecture.py
+++ lecture.py
@@ -362,5 +362,46 @@ class BinarySearchTree:
         elif key > node.key:
             node.right = self._delete(node.right, key)
 
+# Step 34
+
+# The conditionals you wrote in the previous steps are valid for nodes with either zero or one child. 
+# The upcoming conditionals will be valid if there are 2 children.
+
+# For that, add an else clause to the conditional.
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            pass  
+
+# Step 35
+
+# Inside the else block, replace pass with an if statement to check if the left child of the current node is None.
+
+# When node.left is None, there is no left child. Therefore, return the right child from the new if block as a 
+# replacement.
+
+        if node.left is None:
+            return node.right
+
+# Step 36
+
+# Below the if statement, add an elif clause to check if the right child of the current node (node.right) is None.
+
+# If the previous condition is met, it means there is no right child. So, return the left child of the current 
+# node from the elif block as a replacement.
+
+        if node.left is None:
+            return node.right
+        elif node.right is None:
+            return node.left
+
+
+
 
 
