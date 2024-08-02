5955f0525b6f5b6c8ab49966078f2479b7f06663
creinis <115181527+creinis@users.noreply.github.com>
2024-08-02 09:48:53 +0200

    feat: initialize the left and right attributes of the node to None. When a node is first created, it doesn't have any left or right children

--- lecture.py
+++ lecture.py
@@ -45,7 +45,17 @@ class TreeNode:
     def __init__(self, key):
         self.key = key
 
+# Step 5
 
+# Inside the __init__ method, initialize the left and right attributes of the node to None. 
+# This is because when a node is first created, it doesn't have any left or right children. 
+# Remember to use the self keyword.
+
+class TreeNode:    
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
 
 
