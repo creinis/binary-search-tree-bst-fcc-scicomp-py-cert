209f06472738209180fb46a5b58ed49ac4548743
creinis <115181527+creinis@users.noreply.github.com>
2024-08-02 09:47:47 +0200

    feat: set the key attribute

--- lecture.py
+++ lecture.py
@@ -33,6 +33,18 @@ class TreeNode:
     def __init__(self, key):
         pass
 
+# Step 4
+
+# Inside the __init__ method, delete pass and assign the value of the key parameter to the key attribute of the 
+# node using self.key.
+
+# This means that the key attribute of the TreeNode instance will be set to the value passed during the 
+# object's creation.
+
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+
 
 
 
